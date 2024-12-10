b82246b5f28033485b9cb10aa3ecfc324fba89d4
deveshsingh641 <deveshsingh20666@gmail.com>
2024-12-10 13:09:19 +0530

    commitmyfiles

---
+++ script.js
@@ -0,0 +1,39 @@
+// function SayHello(){alert("Hello World!")}
+// document.write("this is my first javascript page")
+
+// var pi=3.14;
+// var person="are you 60";
+// var answer="yes i am!";
+// document.write(pi+ "<br>");
+// document.write(person+ "<br>");
+// document.write(answer+ "<br>");
+
+
+// function myfunction()
+// {
+//     var carname="wlcome";
+//     document.getElementById("message").innerText = "click the button to create a variable, and display the result";
+// }
+
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num="false";
+
+var num=100;
+console.log(num);
+console.log(typeof(num));
+num="false";
+
+var num=16;
+console.log(num);
+console.log(typeof(num));
+num="false";
+
+var num=18;
+console.log(num);
+console.log(typeof(num));
+num="false";
+
+
+
