59d259d4a5d348b5f25c6ada5e3f74e1c8857364
deveshsingh641 <deveshsingh20666@gmail.com>
2024-12-26 12:20:43 +0530

    arrays

--- script.js
+++ script.js
@@ -15,25 +15,111 @@
 //     document.getElementById("message").innerText = "click the button to create a variable, and display the result";
 // }
 
-var num=10;
-console.log(num);
-console.log(typeof(num));
-num="false";
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num="false";
 
-var num=100;
-console.log(num);
-console.log(typeof(num));
-num="false";
+// var num=100;
+// console.log(num);
+// console.log(typeof(num));
+// num="false";
 
-var num=16;
-console.log(num);
-console.log(typeof(num));
-num="false";
+// var num=16;
+// console.log(num);
+// console.log(typeof(num));
+// num="false";
 
-var num=18;
-console.log(num);
-console.log(typeof(num));
-num="false";
+// var num=18;
+// console.log(num);
+// console.log(typeof(num));
+// num="false";
+
+// var count;
+// document.write("<br>Starting Loop :--- <br>");
+// for(count = 0;count<10;count++){
+//     document.write("Current count : " , count);
+//     document.write("<br>");
+// }
+// document.write("Loop stop");
+
+// for(var i=0;i<100;i++){
+//     if (i%2==0){
+//         console.log(i + "is an even number");
+//     }
+//     else{
+//         console.log(i + "is an odd number")
+//     }
+// }
+
+
+
+// var myArray=["A" ,"B" , "C"];
+// for (var i=-0;i<myArray.length;i++)
+// {
+//     console.log("the member of myArray in index" + i + "is" + myArray[i]);
+// }
+
+// var count =0;
+// document.write("starting loop");
+
+// while (count<10){
+//     document.write("current count :" + count + "<br/>");
+//     count++;
+// }
+
+// document.write("loop stopped");
+
+
+// array
+// var my_collection =[  1, "meera naam",
+// true,
+// {
+//     name:"sanjeev",
+//     age: 30
+    
+// },
+
+// function(name){
+//     console.log(name);
+// },
+
+// "abhishek" , "amitabh" ,"bachhan"]
+
+// console.log(my_collection)
+
+
+// function information(firstname , lastname ,language){
+//     if(arguments.length===3){
+//         console.log(firstname)
+//         console.log(lastname)
+//         console.log(language)
+//     }
+
+// }
+
+// information();
+// information("devesh" ,"singh" ,"harsh")
+
+
+// function over() {
+//     document.getElementById("mytext").style.color = "#000"; 
+// }
+
+
+// function over() {
+//     document.getElementById("mytext").style.color = "magneta"; 
+// }
+
+
+
+var retVal = prompt("Enter your name:", "devesh");
+alert("You have entered: " + retVal);
+
+var name = prompt("Enter your name:", "name");
+document.write("<h2>Hello " + name + "</h2>");
+
+
 
 
 
